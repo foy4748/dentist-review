c21494887ba617c6a271fe61e1bcd51263fce267
unknown <faisaljfcl@gmail.com>
2022-11-08 10:47:17 +0600

    Constructed Service Cards

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,33 @@
+//import { Link } from "react-router-dom";
+export default function ServiceCard({ details }) {
+  const { title, price, description, _id } = details;
+  return (
+    <div className="d-flex flex-column justify-content-between min-card-height border rounded">
+      <picture>
+        <img src={`/${title}.jpg`} alt="" className="cardImg" />
+      </picture>
+
+      <div className="p-3">
+        <h2>{title}</h2>
+        <p>
+          Price: <strong>$ {price}</strong>
+        </p>
+        <h3>Brief</h3>
+        <p className="text-justify">
+          {description.length <= 100
+            ? description
+            : `${description.slice(0, 99)} ...`}
+        </p>
+      </div>
+      <button className="cardBtn"> Read More </button>
+    </div>
+  );
+}
+
+/*
+ *
+      <Link to={`/sercies/${_id}`}>
+
+      </Link>
+ *
+*/
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,17 +2,48 @@ import { useEffect, useState } from "react";
 const SERVER =
   import.meta.env.VITE_SERVER_ADDRESS || import.meta.env.VITE_DEV_SERVER;
 
+import { Container, Row, Col } from "react-bootstrap";
+//import Loader from "../Shared/Loader";
+import Loader from "./Loader";
+import ServiceCard from "./ServiceCard";
 export default function Services() {
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch(`${SERVER}/services`)
-      .then((res) => res.json())
-      .then(({ data }) => setServices(data));
+    document.title = "Dr. Hakim || Services";
+    try {
+      fetch(`${SERVER}/services`)
+        .then((res) => res.json())
+        .then(({ data }) => {
+          setServices(data);
+          setLoading(false);
+        });
+    } catch (error) {
+      alert("NETWORK ERROR");
+      setLoading(false);
+    }
   }, []);
+  if (loading) {
+    return <Loader />;
+  }
+
+  const serviceCardsJSX = (obj) => {
+    return <ServiceCard details={obj} key={obj._id} />;
+  };
   return (
     <div>
       <h1>Services</h1>
-      <p>{JSON.stringify(services)}</p>
+      <Container>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {services || !services.error ? (
+            services.map((item) => {
+              return <Col> {serviceCardsJSX(item)}</Col>;
+            })
+          ) : (
+            <Loader />
+          )}
+        </Row>
+      </Container>
     </div>
   );
 }
